f216338b3a8d4a4d38e98f4d4ad8d2e224820e9c
lemuecke <simonklimaschka@t-online.de>
2018-09-14 00:08:29 +0200

    Now running with tensors and FIFO_Queue, but still not as fast as numpy and very memory consuming

--- agent.py
+++ agent.py
@@ -14,7 +14,9 @@ import h5py
 class DQN():
 
     def __init__(self, state_size, action_size):
-        self.sess = tf.Session()
+        self.sess_train = tf.Session()
+        self.sess_infer = tf.Session()
+        self.queue = tf.FIFOQueue(capacity=100, dtypes=(tf.int8, tf.int8))
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
@@ -25,7 +27,6 @@ class DQN():
         self.learning_rate = 0.00025
         self.rho = 0.95
         self.model = self.generate_model()
-        self.queue = tf.FIFOQueue(capacity=100, dtypes=(tf.int8, tf.int8))
 
     def remember(self, state, action, reward, done):
         self.memory.append((state, action, reward, done))
@@ -33,9 +34,37 @@ class DQN():
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        state = tf.slice(state, [0, 0, 0, 0], [4, 105, 80, 1])
-        act_values = self.model.predict(state)
+        #state = tf.slice(state, [0, 0, 0, 0], [4, 105, 80, 1])
+        state = tf.constant(np.array(state).reshape(4, 105, 80, 1), dtype=tf.int8)
+        act_values = self.model.predict(state, steps=1)
         return np.argmax(act_values[0])  # returns action
+
+    def fill_queue(self):
+
+        batch_size = 1
+
+        with tf.name_scope("queue_fill"):
+            def inner_fill():
+                ##while len(self.memory) < batch_size:
+                 #   time.sleep(1)
+
+                state, action, reward, done = random.sample(self.memory, batch_size)[0]
+                target = reward
+                if not done:
+                    state_tbp = tf.slice(state, [1, 0, 0, 0], [4, 105, 80, 1])
+                    target = reward + self.gamma * np.amax(self.model.predict(state_tbp, steps=1)[0])
+                state_f = tf.slice(state, [0, 0, 0, 0], [4, 105, 80, 1])
+                target_f = self.model.predict(state_f, steps=1)
+                target_f[0][action] = target
+
+                return self.queue.enqueue((state_f, tf.constant(target_f, dtype=tf.int8)))
+
+
+            number_of_threads = 4
+            qr = tf.train.QueueRunner(self.queue, [inner_fill()] * number_of_threads)
+            tf.train.add_queue_runner(qr)
+
+        return self.queue.dequeue()
 
     def replay(self, batch_size):
 
@@ -44,39 +73,15 @@ class DQN():
         writer.add_graph(tf.get_default_graph())
 
         replay_start_t = int(round(time.time() * 1000))
-        minibatch = random.sample(self.memory, batch_size)
-        print("Replay1:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-        #minibatch = self.queue.dequeue(batch_size=batch_size)
 
-        for state, action, reward, done in minibatch:
-            target = reward
-            print("Replay2:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-            if not done:
-                states_to_be_predicted = tf.slice(state, [1, 0, 0, 0], [4, 105, 80, 1])
-                print("Replay3:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-                #states_to_be_predicted = state[1:5].reshape(4, 1, 105, 80)
-                target = reward + self.gamma * np.amax(self.model.predict(states_to_be_predicted, steps=1)[0])
-                print("Replay4:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-            state_f = tf.slice(state, [0, 0, 0, 0], [4, 105, 80, 1])
-            print("Replay5:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-            target_f = self.model.predict(state_f, steps=1)
-            print("Replay6:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-            target_f[0][action] = target
-            print("Replay7:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-            target_f = tf.constant(target_f, dtype=tf.int8)
-            print("Replay8:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
-
-            self.queue.enqueue((state_f, target_f))
-            print("Replay9:" + str(int(round(time.time() * 1000)) - replay_start_t), end=",", flush=True)
+        state_f, target_f = self.queue.dequeue()
+        a = 3
+        self.model.fit(state_f, target_f, epochs=1, verbose=0, steps_per_epoch=1)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
         print("ReplayE:" + str(int(round(time.time() * 1000)) - replay_start_t), flush=True)
-
-    def replay_train(self):
-        state_f, target_f = self.queue.dequeue()
-        self.model.fit(state_f, target_f, epochs=1, verbose=0, steps_per_epoch=1)
 
     def generate_model(self):
 
@@ -137,55 +142,63 @@ def train(episodes):
 
     done = False
     batch_size = 32
+    first_start = True
+    coord = tf.train.Coordinator()
 
-    #agent.load("./breakoutDeterministicV4.h5")
-    try:
-        for e in range(episodes):
-            episode_start_t = int(round(time.time() * 1000))
-            state = preprocess(env.reset())
-            action = None
+    with tf.Session() as sess:
 
-            for time_t in range(100000):
+        #agent.load("./breakoutDeterministicV4.h5")
+        try:
+            for e in range(episodes):
+                episode_start_t = int(round(time.time() * 1000))
+                state = preprocess(env.reset())
+                action = None
 
-                frame_collector = list()
-                #reward_collector = list()
-                if time_t == 0:
-                    frame_collector.append(state)
-                else:
-                    action = agent.act(state)
-                    next_state, reward, done, _ = env.step(action)
-                    reward = transform_reward(reward)
-                    next_state = preprocess(next_state)
-                    state.append(next_state)
+                for time_t in range(100000):
 
-                    agent.remember(tf.constant(np.array(state).reshape(5, 105, 80, 1), dtype=tf.int8),
-                                   action, reward, done)
-                    #agent.queue.enqueue((tf.constant(np.array(state).reshape(5, 1, 105, 80), dtype=tf.int8),
-                    #                     action, reward, tf.constant(done, dtype=tf.int8)))
+                    frame_collector = list()
+                    #reward_collector = list()
+                    if time_t == 0:
+                        frame_collector.append(state)
+                    else:
+                        action = agent.act(state)
+                        next_state, reward, done, _ = env.step(action)
+                        reward = transform_reward(reward)
+                        next_state = preprocess(next_state)
+                        state.append(next_state)
 
-                    frame_collector.append(next_state)
-                for frame in range(3):
-                    next_state, reward, done, _ = env.step(0)   #TODO: Check if 0 is really "not moving anywhere"
-                    frame_collector.append(preprocess(next_state))
-                    #reward_collector.append(transform_reward(reward))
+                        agent.remember(tf.constant(np.array(state).reshape(5, 105, 80, 1), dtype=tf.int8),
+                                       action, reward, done)
+                        #agent.queue.enqueue((tf.constant(np.array(state).reshape(5, 1, 105, 80), dtype=tf.int8),
+                        #                     action, reward, tf.constant(done, dtype=tf.int8)))
 
-                state = frame_collector.copy()
+                        frame_collector.append(next_state)
+                    for frame in range(3):
+                        next_state, reward, done, _ = env.step(0)   #TODO: Check if 0 is really "not moving anywhere"
+                        frame_collector.append(preprocess(next_state))
+                        #reward_collector.append(transform_reward(reward))
 
-                #env.render()
+                    state = frame_collector.copy()
 
-                if done:
-                    # print the score and break out of the loop
-                    print("episode: {}/{}, score: {}"
-                          .format(e, episodes, time_t))
-                    break
-                if len(agent.memory) > batch_size:
-                    agent.replay(batch_size)
+                    #env.render()
 
-            print("Episode took " + str(int(round(time.time() * 1000)) - episode_start_t))
+                    if done:
+                        # print the score and break out of the loop
+                        print("episode: {}/{}, score: {}"
+                              .format(e, episodes, time_t))
+                        break
+                    if len(agent.memory) > batch_size:
+                        if first_start:
+                            agent.fill_queue()
+                            threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+                            first_start = False
+                        agent.replay(batch_size)
 
-        agent.save("./breakoutDeterministicV4.h5")
-    except KeyboardInterrupt:
-        agent.save("./breakoutDeterministicV4.h5")
+                print("Episode took " + str(int(round(time.time() * 1000)) - episode_start_t))
+
+            agent.save("./breakoutDeterministicV4.h5")
+        except KeyboardInterrupt:
+            agent.save("./breakoutDeterministicV4.h5")
 
 
 train(50000)
